d5b7677f522a00541f0a2894e273d3a3c55bc59d
ds-gits-it <talentedmrl0real@gmail.com>
2026-07-07 11:57:38 +0530

    refactor: move styles to a separate file for better organization

--- src/components/ui/chat/input.tsx
+++ src/components/ui/chat/input.tsx
@@ -1,4 +1,5 @@
-import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+import styles from '@/style/chat/input';
+import { Pressable, TextInput, View } from 'react-native';
 
 export default function Input() {
   return (
@@ -14,43 +15,3 @@ export default function Input() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 12,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    backgroundColor: 'white',
-  },
-  input: {
-    flex: 1,
-    minHeight: 48,
-    paddingHorizontal: 16,
-    borderRadius: 999,
-    backgroundColor: 'white',
-    borderColor: 'grey',
-    color: 'black',
-    fontSize: 16,
-  },
-  button: {
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'black',
-    color: 'white',
-  },
-  arrow: {
-    width: 12,
-    height: 12,
-    borderTopWidth: 2,
-    borderRightWidth: 2,
-    borderTopColor: 'white',
-    borderRightColor: 'white',
-    transform: [{ rotate: '45deg' }],
-    marginLeft: -2,
-  },
-});
---
+++ src/style/chat/input.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    backgroundColor: 'white',
+  },
+  input: {
+    flex: 1,
+    minHeight: 48,
+    paddingHorizontal: 16,
+    borderRadius: 999,
+    backgroundColor: 'white',
+    borderColor: 'grey',
+    color: 'black',
+    fontSize: 16,
+  },
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  arrow: {
+    width: 12,
+    height: 12,
+    borderTopWidth: 2,
+    borderRightWidth: 2,
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    transform: [{ rotate: '45deg' }],
+    marginLeft: -2,
+  },
+});
