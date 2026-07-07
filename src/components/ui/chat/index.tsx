971455444c2fab2d6b8daf0c3e9a0e1d4d47cf75
ds-gits-it <talentedmrl0real@gmail.com>
2026-07-07 12:17:46 +0530

    feat: add reset functionality to clear chat history on button press

--- src/components/ui/chat/index.tsx
+++ src/components/ui/chat/index.tsx
@@ -1,11 +1,16 @@
 import ChatHistory from '@/components/ui/chat/chat-history';
 import Input from '@/components/ui/chat/input';
 import TopBar from '@/components/ui/chat/topbar';
+import storage from '@/store';
 import { View } from 'react-native';
 
 export default function ChatUI() {
   const eventHandler = () => {
     console.log('Reset button pressed');
+    const run = async () => {
+      await storage.setItem('chat-history', JSON.stringify([]));
+    };
+    run();
   };
   return (
     <View style={{ flex: 1 }}>
